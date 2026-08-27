faf4d64c9b4067503e1a968e8dcac9bf487350a3
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-28 02:27:41 +0530

    Delete tests directory

--- tests/test_milestone33_catalog.py
+++
@@ -1,5 +0,0 @@
-from src.question_catalog import load_catalog
-
-
-def test_empty_catalog(tmp_path):
-    assert load_catalog(tmp_path) == {"questions": []}
--- tests/test_milestone33_duplicate.py
+++
@@ -1,5 +0,0 @@
-from src.question_duplicate_detector import fingerprint
-
-
-def test_same_text_same_hash():
-    assert fingerprint("abc") == fingerprint("abc")
